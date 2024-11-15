483e456df2d30846f02e510087792848dd6c4e71
sebastian quesada <sebastianquesadarios@gmail.com>
2024-11-15 02:14:18 -0500

    Correcion del cors

--- index.js
+++ index.js
@@ -5,13 +5,21 @@ const router = require('./routes/cliphub.routes.js');
 const cors = require('cors');
 require('dotenv').config();
 
-
 const app = express();
+
+// Configuración de CORS
+const corsOptions = {
+    origin: ['http://localhost:5173', 'https://front-clip-hub.vercel.app/'], // Cambia 'https://tudominio.com' por tu dominio en producción
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+// Aplicar CORS con las opciones configuradas
+app.use(cors(corsOptions));
 
 // Middleware para analizar datos codificados y JSON
 app.use(urlencoded({ extended: true }));
 app.use(json());
-app.use(cors());
 
 // Manejador para la ruta raíz
 app.get('/', (req, res) => {
@@ -24,7 +32,7 @@ app.get('/v1/signos', (req, res, next) => {
     next();
 });
 
-
+// Usar las rutas de ClipHub
 app.use('/v1/ClipHub', router);
 
 // Iniciar el servidor
@@ -32,3 +40,4 @@ const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`);
 });
+
