21251dab0e39cd08ed26d7aadf44918eb5583f59
sebastian quesada <sebastianquesadarios@gmail.com>
2024-11-15 14:22:05 -0500

    21

--- routes/cliphub.routes.js
+++ routes/cliphub.routes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const router = express.Router();
-const { login, register } = require('./controllers/cliphubControllers');
+const { login, register, authenticateToken } = require('./controllers/cliphubControllers');
 const uploadMiddleware = require('../database/uploadMiddleware'); // Asegúrate de que esté apuntando correctamente
 const videoController = require('./controllers/videoController'); // Controlador para gestionar la subida de videos
-const authenticateToken = require('../path/to/authenticateToken');  // Ajusta el path según sea necesario
+
 
 
 // Cargar las variables de entorno correctamente
