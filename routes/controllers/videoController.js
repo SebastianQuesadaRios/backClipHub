d53283c9b1400b8f42afa7b1914fceb028f0ce2e
sebastian quesada <sebastianquesadarios@gmail.com>
2024-11-16 22:40:36 -0500

    1

--- routes/controllers/videoController.js
+++ routes/controllers/videoController.js
@@ -3,10 +3,10 @@ const { connectDb, getDb } = require('../../database/mongo');
 const moment = require('moment-timezone');
 
 const uploadVideo = async (req, res) => {
-    const { title, description, email } = req.body;
+    const { title, description } = req.body;
 
     // Verifica que los archivos y los campos obligatorios estén presentes
-    if (!req.files || !req.files.video || !req.files.preview || !title || !description || !email) {
+    if (!req.files || !req.files.video || !req.files.preview || !title || !description) {
         return res.status(400).json({ status: "Error", message: "Faltan campos obligatorios o los archivos" });
     }
 
@@ -15,31 +15,21 @@ const uploadVideo = async (req, res) => {
         const previewUrl = req.files.preview[0].location; // URL de la imagen de preview subida a S3
 
         const db = await connectDb(); // Conectar a la base de datos
-
-        // Buscar al usuario por su email
-        const user = await db.collection('users').findOne({ email });
-        if (!user) {
-            return res.status(404).json({ status: "Error", message: "Usuario no encontrado" });
-        }
-
-        // Crear el nuevo video
         const newVideo = {
             title,
             description,
             videoUrl, // URL del video
             previewUrl, // URL de la imagen de preview
             uploadDate: moment().format(),
-            userId: user._id, // Guardamos el ID del usuario
         };
 
         // Guardar el video en la base de datos
         await db.collection('videos').insertOne(newVideo);
-
-        res.status(201).json({
-            status: "Éxito",
-            message: "Video y preview subidos exitosamente",
-            videoUrl,
-            previewUrl
+        res.status(201).json({ 
+            status: "Éxito", 
+            message: "Video y preview subidos exitosamente", 
+            videoUrl, 
+            previewUrl 
         });
     } catch (error) {
         console.error('Error al subir el video y el preview:', error);
