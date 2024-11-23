dba8477dba53abe947117a7f98fb091777dc376a
sebastian quesada <sebastianquesadarios@gmail.com>
2024-11-23 16:01:57 -0500

    3

--- index.js
+++ index.js
@@ -6,11 +6,17 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+// Configuración de CORS
+const corsOptions = {
+    origin: ['https://front-clip-hub.vercel.app'], // Solo permite este dominio
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+};
+app.use(cors(corsOptions));
 
-// Middleware para analizar datos codificados y JSON
-app.use(urlencoded({ extended: true }));
-app.use(json());
+// Middleware para analizar datos codificados y JSON con límites más altos
+app.use(json({ limit: '1gb' })); // Aumenta el límite del cuerpo JSON a 1 GB
+app.use(urlencoded({ limit: '1gb', extended: true })); // Aumenta el límite para datos de formularios
 
 // Ruta raíz
 app.get('/', (req, res) => {
@@ -24,4 +30,5 @@ const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`);
 });
+
 
